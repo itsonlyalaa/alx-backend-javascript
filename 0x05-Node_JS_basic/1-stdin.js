f8c833c6a37d535b9a7a89f97f1e3e4e142837af
Alaa Okair <alaaokair330@gmail.com>
2024-11-27 02:16:40 +0000

    task1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,11 +1,9 @@
-console.log('Welcome to Holberton School, what is your name?\n');
-process.stdin.resume();
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('readable', () => {
- const name = process.stdin.read();
- if (process.stdin.isTTY) {
-     process.exit();
-} else {
-  process.stdout.write('This important software is now closing\n');
-  process.exit();
-}
+	  const name = process.stdin.read();
+	  if (name) process.stdout.write(`Your name is: ${name}`);
+});
+
+process.stdin.on('close', () => {
+	  process.stdout.write('This important software is now closing\n');
 });
